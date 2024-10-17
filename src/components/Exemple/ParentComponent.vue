e0f54618a17b78141a30759de9646574fe4f64da
sebastienseenovate <sebastien.cruz@seenovate.com>
2024-10-17 08:59:12 +0200

    add a parent component to use databox on html page

---
+++ src/components/Exemple/ParentComponent.vue
@@ -0,0 +1,75 @@
+/* eslint-disable */
+<template>
+  <div>
+    <!-- Utilisation du composant DataBox -->
+    <DataBox :serieObj="serieObj" :modalSettings="modalSettings"
+      @select-source-api="handleSelectSourceApi" @open-modal="openModal"></DataBox>
+    <!-- Modale -->
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import DataBox from '../DataBox.vue'
+
+export default {
+  name: 'ParentComponent',
+  components: { DataBox },
+  data() {
+    return {
+      modalSettings: {
+        hasModal: false,
+        modalId: 'fr-theme-modal', // Assurez-vous que l'ID est unique
+      },
+      serieObj: {
+        title: "Titre de la dataBox",
+        description: "Description de la dataBox.",
+        indicator: false,
+        trendValue: "5",
+        value: "1500",
+        component: "PieChart",
+        add_sources: false,
+        option_default: "ubm",
+        select_options: [
+          { value: "ubm", label: "Exposition médiatique" },
+          // Autres options
+        ],
+        update_date: "22-04-2024",
+        source: "SIG",
+        showGraph: true,
+        serie_values: {
+          x: ["Serie 1", "Serie 2", "Serie 3"],
+          y: [100, 200, 300],
+        },
+        table: [
+          ["Serie", "Valeur"],
+          ["Serie1", "100"],
+          ["Serie2", "200"],
+          ["Serie3", "300"],
+        ],
+      },
+    };
+  },
+  methods: {
+    handleSelectSourceApi(selectedOption) {
+      console.log('Option sélectionnée :', selectedOption);
+    },
+    openModal() {
+      if (this.modalInstance) {
+        this.modalInstance.show();
+      }
+    },
+    closeModal() {
+      if (this.modalInstance) {
+        this.modalInstance.hide();
+      }
+    },
+  },
+  mounted() {
+    if (this.$refs.modal) {
+      // Initialize the modal instance
+      this.modalInstance = new Modal(this.$refs.modal);
+    }
+  },
+};
+</script>
