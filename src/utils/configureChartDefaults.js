0df9e0501cc0553cf61727ff014199ce8f30008a
sebastienseenovate <sebastien.cruz@seenovate.com>
2024-11-28 16:02:39 +0100

    Create configureChartDefault file and clean charts from default value

---
+++ src/utils/configureChartDefaults.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { Chart } from 'chart.js';
+
+export const configureChartDefaults = () => {
+  Chart.defaults.global.defaultFontFamily = 'Marianne';
+  Chart.defaults.global.defaultFontSize = 12;
+  Chart.defaults.global.defaultLineHeight = 1.66;
+  Chart.defaults.global.defaultFontColor = '#DDDDDD';
+};
