bc8b56ea3b6f0e61d09da28fecf033638e224869
QuentiumYT <quentin.lienhardt@gmail.com>
2025-01-22 10:04:50 +0100

    Use only icons CSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import '../node_modules/@gouvfr/dsfr/dist/dsfr.module.min.js';
 import '../node_modules/@gouvfr/dsfr/dist/dsfr.main.min.css';
-import '../node_modules/@gouvfr/dsfr/dist/utility/utility.main.min.css';
+import '../node_modules/@gouvfr/dsfr/dist/utility/icons/icons.main.min.css';
 
 import '@/charts/main.js';
 import '@/styles/style.scss';
