e2b9e42bcd612a93730f4d639737418f9b766094
QuentiumYT <quentin.lienhardt@gmail.com>
2026-02-20 15:00:27 +0100

    Script to migrate english countries to french

---
+++ src/utils/alter_countries.js
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+
+import { WORLD } from './constants';
+
+function parseCsv(content) {
+  const lines = content
+    .trim()
+    .split(/\r?\n/)
+    .filter((l) => l.length > 0);
+  if (lines.length === 0) return [];
+  const headers = lines[0].split(',');
+  return lines.slice(1).map((line) => {
+    const cols = line.split(',');
+    const row = {};
+    headers.forEach((h, i) => {
+      row[h] = cols[i] || '';
+    });
+    return row;
+  });
+}
+
+const csvPath = path.join(__dirname, 'countries.csv');
+const csvData = fs.readFileSync(csvPath, { encoding: 'utf-8' });
+const countries = parseCsv(csvData);
+const continents = {
+  Asia: 'Asie',
+  Europe: 'Europe',
+  Africa: 'Afrique',
+  'South America': 'Amérique du Sud',
+  Oceania: 'Océanie',
+  'North America': 'Amérique du Nord',
+  Antarctica: 'Antarctique',
+};
+const countryOverrides = {
+  MD: 'Moldavie',
+};
+
+const FRENCH_WORLD = [];
+
+WORLD.forEach((country) => {
+  const match = countries.find((c) => c['Alpha-2 code'] === country.country_value);
+  if (match) {
+    FRENCH_WORLD.push({
+      country: countryOverrides[country.country_value] || match['French short name'].split(' (')[0].replace("'", '’'),
+      country_value: country.country_value,
+      continent: continents[country.continent] || country.continent,
+      continent_value: country.continent_value,
+    });
+  }
+});
+
+fs.writeFileSync(path.join(__dirname, 'countries_fr.js'), JSON.stringify(FRENCH_WORLD, null, 2), { encoding: 'utf-8' });
